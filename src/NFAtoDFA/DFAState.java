084074fb78fbaffa9a66f2d09eafe9c4d201e99c
avigeris <dascha2611@gmail.com>
2019-12-09 21:16:13 +0200

    Initial commit

---
+++ src/NFAtoDFA/DFAState.java
@@ -0,0 +1,26 @@
+package NFAtoDFA;
+import java.util.ArrayList;
+import java.util.*;
+
+public class DFAState {
+    ArrayList<Integer> startStates, endStates;
+    String symbol;
+
+    DFAState(ArrayList<Integer> startStates, String symbol, ArrayList<Integer> endStates){
+        this.startStates = startStates;
+        this.symbol = symbol;
+        this.endStates = endStates;
+    }
+
+
+    public boolean equalToStart(ArrayList<Integer> find){
+        int matches = 0;
+        if(find.size() != startStates.size()) return false;
+        for(int g=0; g< find.size(); ++g){
+            if(startStates.contains(find.get(g))) matches++;
+        }
+        return matches == find.size();
+    }
+
+}
+
